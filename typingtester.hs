0ebc302080d9cae6a8e99c5bb03294366f41f48f
Alan Hawkins <alanpika_au@yahoo.com>
2013-03-02 16:35:46 +1100

    Update typingtester.hs
    fixed time aquisition

--- typingtester.hs
+++ typingtester.hs
@@ -4,19 +4,23 @@ import System.Time
 import Text.Printf
 import Control.Monad
 import System.IO
+import System.Posix.Unistd
+import Data.Time
 
 hPutStrFlush h str = hPutStr h str >> hFlush h 
 
-main = forever $ do
-         hPutStrFlush stdout "type something:"
-	 t1 <- time
-	 line <- getLine
-	 t2 <- time
-	 let numWords = (genericLength . words  $ line :: Float)
-	 let tdiff = difftimes t1 t2
-	 let timeinunits =  (/100000000000) . fromIntegral . tdPicosec $ tdiff
-	 let speed = numWords / (timeinunits/60)
-	 putStrLn (printf "%d words in %f seconds for a speed of %f WPM (words per minute)" (round numWords :: Int) timeinunits speed )
-	 
-time = getClockTime 
-difftimes t1 t2 = diffClockTimes t1 t2
+main = 
+  forever $ do
+     -- prompt user to type
+     hPutStrFlush stdout "type something:"
+     -- grab time 
+     t1 <- getCurrentTime
+     -- grab input
+     line <- getLine
+     -- grab time 
+     t2 <- getCurrentTime
+     --print 2
+     let timeinunits = ( realToFrac $ diffUTCTime t2 t1 :: Float) 
+     let numWords = ( genericLength . words  $ line :: Float)
+     let speed = numWords / (timeinunits/60)
+     putStrLn (printf "%d words in %f seconds for a speed of %f WPM (words per minute)" (round numWords :: Int) timeinunits speed )
