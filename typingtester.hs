a2e014e1f000c9b19f3303ea6f8bb6a4093fa541
alan hawkins <hawk.alan@gmail.com>
2014-08-17 22:15:24 +1000

    change

--- typingtester.hs
+++ typingtester.hs
@@ -3,14 +3,29 @@ import Data.List
 import Data.Time
 import System.IO
 import Text.Printf
+import System.Environment
+import System.Directory 
+import Data.Monoid
 
 hPutStrFlush h str = putStr str >> hFlush h 
 
-
-main = 
-  forever $ do
+main = do
+  a <- getArgs
+  let target_phrase = (concat $ intersperse " " a)
+  let target_phrase_set = not . null $ target_phrase 
+  d <- getAppUserDataDirectory "typingTester"
+  createDirectoryIfMissing True d
+  let file = (d++"/highscores.txt")
+  file_exists <- doesFileExist file
+  {-
+  a <- if file_exists then read . readFile exists 
+                      else return []
+ -}
+  when target_phrase_set
+    (putStrLn ("target phrase: " ++ target_phrase))
+  foreverWith (0) $ \(x) -> do
      -- prompt user to type
-     hPutStrFlush stdout "type something:"
+     hPutStrFlush stdout (show x++": type something:")
      -- grab time 
      t1 <- getCurrentTime
      -- grab input
@@ -21,4 +36,11 @@ main =
      let timeinunits = ( realToFrac $ diffUTCTime t2 t1 :: Float) 
      let numWords = ( genericLength . words  $ line :: Float)
      let speed = numWords / (timeinunits/60)
-     putStrLn (printf "%d words in %f seconds for a speed of %f WPM (words per minute)" (round numWords :: Int) timeinunits speed )
+     let match = line == target_phrase
+     if target_phrase_set && (not match) then putStrLn "failure to match"
+                                         else putStrLn (printf "%d words in %f seconds for a speed of %f WPM (words per minute)" (round numWords :: Int) timeinunits speed )
+     return (x+1)
+
+foreverWith a m = do 
+   b <- m a
+   foreverWith b m
